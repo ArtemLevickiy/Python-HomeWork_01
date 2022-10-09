d573596f387f00c0a394513d5c1a8d6545d65399
Artem <rrampage1991@gmail.com>
2022-10-09 18:56:04 +0300

    Task_002_re

--- Task_002.py
+++ Task_002.py
@@ -2,28 +2,9 @@
 # Используйте знания об Алгебра Логике, вы должны перебрать все возможные комбинации значений X, Y, Z (принимают значения 0 или 1)
 
 
-# Я не разобрался с условием задачи, поэтому нашел решение в интернете,
-# если пойму, переделаю((
-
-
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
-
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+print('x\ty\tz')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            result = not(x or y or z) == (not(x) and not(y) and not(z))
+            print(f'{x}\t{y}\t{z}\t{result}')
